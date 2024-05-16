eee255ab60b6f972b234af9e22a96e22998c51c3
AmanJha1105 <amanjha1105@gmail.com>
2024-05-16 16:15:13 +0530

    user resolvers added

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,48 @@
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import dotenv from "dotenv";
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import{buildContext} from "graphql-passport"
 import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
+
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
 
 dotenv.config();
+configurePassport();
 const app = express();
 
 const httpServer = http.createServer(app);
 
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection:"sessions",
+});
 
-import mergedResolvers from "./resolvers/index.js";
-import mergedTypeDefs from "./typeDefs/index.js";
+store.on("error",(err)=>console.log(err));
+
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge: 1000*60*60*24*7,
+        httpOnly:true,
+    },
+    store:store,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
     typeDefs:mergedTypeDefs,
@@ -28,12 +56,15 @@ await server.start();
 // and our expressMiddleware function.
 app.use(
     '/',
-    cors(),
+    cors({
+        origin:"http://localhost:3000",
+        credentials:true,
+    }),
     express.json(),
     // expressMiddleware accepts the same arguments:
     // an Apollo Server instance and optional configuration options
     expressMiddleware(server, {
-      context: async ({ req }) => ({req}),
+      context: async ({ req,res }) => buildContext({req,res}),
     }),
   );
   
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,14 +1,102 @@
 import { users } from "../dummyData/data.js"
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver ={
-   Query: {
-    users: ()=>{
-        return users
-    },
-    user :(_,{userId})=>{
-        return users.find((user)=>user._id===userId)
+
+   Mutation: {
+     signUp: async(_,{input},context) => {
+        try {
+           const {username,name,password,gender} = input;
+
+           if (!username || !name || !password || !gender) {
+            throw new Error("All fields are required");
+           }
+           
+           const existingUser = await User.findOne({username});
+           if(existingUser){
+            throw new Error("User already exists");
+           }
+
+           const salt= await bcrypt.genSalt(10);
+           const  hashedPassword = await bcrypt.hash(password,salt);
+
+           // https://avatar-placeholder.iran.liara.run/
+            const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+            const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+            const newUser = new User({
+                username,
+                name,
+                password:hashedPassword,
+                gender,
+                profilePicture :gender==="male" ? boyProfilePic : girlProfilePic,
+            })
+
+            await newUser.save();
+            await context.login(newUser);
+            return newUser;
+
+        } catch (error) {
+            console.error("Error in signup:",error);
+            throw new Error(error.message || "Internal Server Error");
+        }
      },
+
+     login: async(_,{input},context)=>{
+        try {
+            const {username,password}=input;
+            await context.authenticate("graphql-local",{username,password})
+
+            await context.login(user);
+            return user;
+
+        } catch (error) {
+            console.error("Error in login:",error);
+            throw new Error(error.message || "Internal Server Error");
+        }
+     },
+
+     logout: async(_,{},context)=>{
+        try {
+            await context.logout();
+
+            req.session.destroy((err)=>{
+                if(err)throw err;
+            })
+
+            res.clearCookie("connect.sid");
+            return {message: "Logged out successfully"};
+
+        } catch (error) {
+            console.error("Error in logout:",error);
+            throw new Error(error.message || "Internal Server Error");
+        }
+      },
+     },
+
+   Query: {
+    
+    authUser : async(_,{},context)=>{
+       try {
+         const user = await context.getUser();
+         return user;
+
+       } catch (error) {
+        console.error("Error in authUser:",error);
+        throw new Error(error.message || "error in authenticating user");
+       }
+    },
+    user :async(_,{userId})=>{
+        try {
+            const user = await User.findById(userId);
+            return user;
+        } catch (error) {
+            console.error("Error in user query:",error);
+            throw new Error(error.message || "Error getting user");
+       }
+    },
    },
-}
+  }
 
 export default userResolver;
