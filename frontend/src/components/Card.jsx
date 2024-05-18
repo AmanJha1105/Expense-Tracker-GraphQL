90587b0d28f31dac59d8c113abbaa0b5cb304d57
AmanJha1105 <amanjha1105@gmail.com>
2024-05-19 00:45:14 +0530

    updating currency symbol Nand bug fixes

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -64,7 +64,7 @@ const Card = ({ transaction, authUser }) => {
 				</p>
 				<p className='text-white flex items-center gap-1'>
 					<FaSackDollar />
-					Amount: ${amount}
+					Amount: ₹{amount}
 				</p>
 				<p className='text-white flex items-center gap-1'>
 					<FaLocationDot />
