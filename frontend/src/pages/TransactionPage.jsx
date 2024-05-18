159b8dab0f570afdf79193717ed1f27a0b8d084c
AmanJha1105 <amanjha1105@gmail.com>
2024-05-18 12:41:24 +0530

    refetchqueries fixed

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,7 +1,7 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { GET_TRANSACTION } from "../graphql/queries/transaction.query";
+import { GET_TRANSACTION, GET_TRANSACTION_STATISTICS } from "../graphql/queries/transaction.query";
 import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
 import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
@@ -14,7 +14,7 @@ const TransactionPage = () => {
 	});
 
 	const [updateTransaction,{loading:loadingUpdate}]=useMutation(UPDATE_TRANSACTION,{
-		refetchQueries: ["GetTransactions","GetTransactionStatistics"],
+		refetchQueries: [{query:GET_TRANSACTION_STATISTICS}],
 	})
 
 
