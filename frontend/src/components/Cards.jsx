0a2ea9ecde3e120f6ac0b5eb852816ab3e344ebf
AmanJha1105 <amanjha1105@gmail.com>
2024-05-18 15:03:56 +0530

    bug fixes in cards

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -23,7 +23,7 @@ const Cards = () => {
 			<p className='text-5xl font-bold text-center my-10'>History</p>
 			<div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-start mb-20'>
 				{!loading &&
-					data.transactions.map((transaction) => (
+					data?.transactions?.map((transaction) => (
 						<Card key={transaction._id} transaction={transaction} authUser={authUser.authUser}/>
 					))}
 			</div>
